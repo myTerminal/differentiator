49fc6e680156aee7144b38fa83264b7563bc8cd9
Mohammed Ismail Ansari <team.terminal@gmail.com>
2018-04-17 15:48:54 +0000

    implement core structure

--- src/scripts/app.jsx
+++ src/scripts/app.jsx
@@ -5,22 +5,116 @@ import './service-worker-starter.js';
 
 const React = require('react');
 const ReactDOM = require('react-dom');
+import { FilePicker } from 'react-file-picker';
+
+import packageDetails from '../../package.json';
 
 const page = document.getElementById('page');
 
 class App extends React.Component {
+
     constructor() {
         super();
 
         this.state = {
-            
+            isLeftFileLoaded: false,
+            isRightFileLoaded: false,
+            leftFileContents: '',
+            rightFileContents: ''
         };
+    }
+
+    onFilePick (file, containerNumber) {
+        this.loadFile(file, containerNumber);
+    }
+
+    loadFile (file, containerNumber) {
+        var context = this,
+            reader = new FileReader();
+
+        reader.onload = function (event) {
+            if (containerNumber === 1) {
+                context.setState({
+                    isLeftFileLoaded: true,
+                    leftFileContents: event.target.result
+                });
+
+                if (context.state.isRightFileLoaded) {
+                    context.startComparingFileContents();
+                }
+            } else if (containerNumber === 2) {
+                context.setState({
+                    isRightFileLoaded: true,
+                    rightFileContents: event.target.result
+                });
+
+                if (context.state.isLeftFileLoaded) {
+                    context.startComparingFileContents();
+                }
+            }
+        };
+
+        reader.readAsText(file);
+    }
+
+    resetComparison () {
+        this.setState({
+            isLeftFileLoaded: false,
+            isRightFileLoaded: false,
+            leftFileContents: '',
+            rightFileContents: ''
+        });
+    }
+
+    startComparingFileContents () {
+        console.log('Comparing file contents');
     }
 
     render() {
         return (
-            <div className="container">
-                differentiator
+            <div className='root-container'>
+                <div className='header'>
+                    <b>differentiator</b> - A simple file compare tool (v {packageDetails.version})
+                    <a className='source' href='https://github.com/myTerminal/differentiator' target='_blank'></a>
+                </div>
+                <div className='container'>
+                    <div className={'file-input' + (this.state.isLeftFileLoaded ? ' loaded' : '')}>
+                        <div className='picker'>
+                            <FilePicker
+                                onChange={file => this.onFilePick(file, 1)}
+                                onError={err => alert(err)}>
+                                <button className='file-picker-button'>
+                                    Pick a file to compare
+                                </button>
+                            </FilePicker>
+                        </div>
+                        <div className='file-contents'>
+                            {this.state.leftFileContents}
+                        </div>
+                    </div>
+                    <div className={'file-input' + (this.state.isRightFileLoaded ? ' loaded' : '')}>
+                        <div className='picker'>
+                            <FilePicker
+                                onChange={file => this.onFilePick(file, 2)}
+                                onError={err => alert(err)}>
+                                <button className='file-picker-button'>
+                                    Pick a file to compare
+                                </button>
+                            </FilePicker>
+                        </div>
+                        <div className='file-contents'>
+                            {this.state.rightFileContents}
+                        </div>
+                    </div>
+                </div>
+                <div className={'footer' + (this.state.isLeftFileLoaded && this.state.isRightFileLoaded ? ' loaded': '')}>
+                    <div className='instruction'>
+                        Pick a file on each side to begin comparing contents
+                    </div>
+                    <div className='control' onClick={this.resetComparison.bind(this)}>
+                        Reset comparison
+                    </div>
+                </div>
             </div>
         );
     }
