2f3b88bdcb5dde0508a04a7d582374e7b0898d9b
myTerminal <team.terminal@gmail.com>
2018-05-01 23:12:42 -0500

    initial commit

---
+++ sw.js
@@ -0,0 +1,57 @@
+/* global caches fetch skipWaiting */
+
+var cacheName = '1525234763001',
+    origin = '/differentiator/';
+
+this.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(
+                [
+                    origin,
+                    origin + 'index.html',
+                    origin + 'styles/styles.css',
+                    origin + 'scripts/app.js',
+                    origin + 'fonts/OpenSans-Light.ttf',
+                    origin + 'fonts/OpenSans-Regular.ttf',
+                    origin + 'fonts/RobotoMono-Bold.ttf',
+                    origin + 'fonts/RobotoMono-Regular.ttf'
+                ]
+            );
+        }).catch(function (err) {
+            console.log(err);
+        })
+    );
+});
+
+this.addEventListener('fetch', function (event) {
+    var urlWithoutQueryParams = event.request.url.split('?')[0];
+
+    event.respondWith(caches.match(urlWithoutQueryParams).then(function (response) {
+        if (response) {
+            return response;
+        }
+
+        return fetch(event.request);
+    }));
+});
+
+this.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(keyList => Promise.all(keyList.map(key => {
+            if (key !== cacheName) {
+                return caches.delete(key);
+            } else {
+                return null;
+            }
+        })))
+    );
+});
+
+this.addEventListener('message', messageEvent => {
+    if (messageEvent.data === 'skipWaiting') {
+        return skipWaiting();
+    } else {
+        return null;
+    }
+});
