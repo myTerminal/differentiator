b1f0dd3f274e1c655c539deac4af573f010c37bf
myTerminal <team.terminal@gmail.com>
2019-01-03 23:37:36 -0600

    move html creation code from webpack.common.js to webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,18 @@
 /* global require module */
 
+const sourceDir = 'src';
+
 const WebpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const html = new HtmlWebpackPlugin({
+    template: sourceDir + '/index.html'
+});
 
 module.exports = WebpackMerge(commonConfig, {
-    devtool: 'inline-source-map'
+    devtool: 'inline-source-map',
+    plugins: [
+        html
+    ]
 });
